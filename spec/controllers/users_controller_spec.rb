7e7ee1c53e90ea27374c7d50d74beea4076ea520
Guilherme Lopes <guilhermelopes@gmail.com>
2011-06-08 09:59:42 -0300

    User model with passwords, and show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,7 +5,7 @@ describe UsersController do
   render_views 
 
   describe "GET 'new'" do
-    it "should be successful" do
+   it "should be successful" do
       get 'new'
       response.should be_success
     end
@@ -15,4 +15,25 @@ describe UsersController do
     end
   end
 
+  describe "GET 'show'" do
+    before(:each) do
+      @user = Factory(:user)
+      get :show, :id => @user
+    end
+    it "should be successful" do
+      response.should be_success
+    end
+    it "sould find the right user" do
+      assigns(:user).should == @user
+    end
+    it "should have the user on title" do
+     response.should have_selector("title", :content => @user.name)
+    end
+    it "should include user on page" do
+      response.should have_selector("h1", :content => @user.name)
+    end
+    it "should have a profile image" do
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
+  end
 end
